b056ebe3c0a4be92ccf220c7fc19c87e19ba298f
Khailas12 <khailas303@gmail.com>
2023-06-16 14:23:48 +0530

    initial

---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DashboardService {
+
+
+  constructor() { }
+  private dashboardData = {
+    "data": [
+      {
+        "name": "No Document Cases",
+        "count": 100
+      },
+      {
+        "name": "Document Received Cases",
+        "count": 50
+      },
+      {
+        "name": "Work In Progress Cases",
+        "count": 50
+      },
+      {
+        "name": "Quality Check Cases",
+        "count": 20
+      },
+      {
+        "name": "Head Cases",
+        "count": 30
+      },
+      {
+        "name": "Committee Review Cases",
+        "count": 40
+      },
+      {
+        "name": "Finalized Cases",
+        "count": 60
+      },
+      {
+        "name": "All Cases",
+        "count": 350
+      }
+    ]
+  };
+
+  getConditionData(): Observable<any> {
+    return of(this.dashboardData);
+  }
+
+}
